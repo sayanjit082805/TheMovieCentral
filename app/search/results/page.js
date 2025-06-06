f517f47a0dfabe430a268f7fd56d10e3007c9471
sayanjit082805 <sayanjitukil2005@gmail.com>
2025-06-06 21:27:27 +0530

    fixes

--- app/search/results/page.js
+++ app/search/results/page.js
@@ -1,5 +1,4 @@
 "use client";
-export const dynamic = "force-dynamic"
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { Nav } from "@/components/navbar";
